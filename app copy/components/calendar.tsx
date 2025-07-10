b5be02c439ebbe416648fa23eb713b3bd03aedec
yug5 <yugshrimali82@gmail.com>
2025-07-10 11:45:02 +0530

    Trays11

--- app copy/components/calendar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Calendar() {
-  return (
-    <div className='bg-white h-full rounded-2xl outline-dashed outline-3 outline-gray-400 p-4 flex flex-col justify-between'>
-     
-    </div>
-  )
-}
