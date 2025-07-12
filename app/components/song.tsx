699691a8172ab7c52208c3382cf6d00cc5c9999f
yug5 <yugshrimali82@gmail.com>
2025-07-12 21:23:24 +0530

    Trays14

--- app/components/song.tsx
+++ app/components/song.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function song() {
   return (
-    <div className='bg-white h-full rounded-2xl outline-dashed outline-3 outline-gray-400 p-4 flex flex-col justify-between'>
-      
+    <div className="bg-white h-full rounded-2xl outline-dashed outline-3 outline-gray-400 p-4 flex flex-col justify-between">
+      <h1>Spotify suggestion</h1>
+      <div className="w-[70%] h-[70%] bg-red-300"></div>
     </div>
-  )
+  );
 }
